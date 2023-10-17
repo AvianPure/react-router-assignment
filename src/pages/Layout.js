5efc075546fb27fa97d3fda68ddd612ebda3f34b
AvianPure <129159138+AvianPure@users.noreply.github.com>
2023-10-17 09:26:50 +0300

    Revert "	modified:   src/pages/Layout.js"
    This reverts commit a9d142eb61842bab60a32430c9dc865d90362014.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,38 +1,24 @@
 import { Outlet, Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Layout = () => {
   return (
     <>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">React SHIT</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/Blogs">
-                blog
-            </NavDropdown.Item>
-              <NavDropdown.Item href="/Contact">
-                contact
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
 
-    <Outlet />
-
+      <Outlet />
     </>
-    
   )
 };
 
